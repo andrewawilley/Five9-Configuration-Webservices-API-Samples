2e4b6755106375ebf9d9b5665b874c4c421089c8
Andrew Willey <andrew.willey@five9.com>
2022-01-04 08:21:25 -0600

    added five9_session with get_client method.  updated readme

---
+++ five9_session.py
@@ -0,0 +1,64 @@
+from lxml import etree
+import requests
+import zeep
+from zeep.plugins import HistoryPlugin
+
+from private.credentials import ACCOUNTS
+
+# This function prints the SOAP envelope for the request and/or response
+def zeep_history_print(history, last_sent=True, last_received=True):
+    try:
+        if last_sent is True:
+            print(etree.tostring(
+                history.last_sent["envelope"], 
+                encoding="unicode", 
+                pretty_print=True))
+        if last_received is True:
+            print(etree.tostring(
+                history.last_received["envelope"],
+                encoding="unicode",
+                pretty_print=True))
+    except (IndexError, TypeError):
+        # noncritical if fails here, pass
+        pass
+
+def get_client():
+    # initialize the zeep history object
+    history = HistoryPlugin()
+
+    # url and user settings consolidated here for convenience to use later
+    settings = {
+        'FIVENINE_CONFIG_WEBSERVICES_API': 
+            'https://api.five9.com/wsadmin/v12/?wsdl&user={five9username}',
+    }
+
+
+    # Target the desired account using the alias in private.credentials
+    api_account_alias = 'default_account'
+    api_account = ACCOUNTS.get(api_account_alias, {})
+
+    if api_account.get('username', None) in [None, 'apiUserUsername']:
+        five9username = input('Enter Username: ')
+        five9password = input('Enter Password: ')
+    else:
+        five9username = api_account.get('username', None)
+        five9password = api_account.get('password', None)
+
+    # prepare the session with BasicAuth headers
+    session = requests.Session()
+    session.auth = requests.auth.HTTPBasicAuth(five9username, five9password)
+    try:
+        client = zeep.Client(
+            settings['FIVENINE_CONFIG_WEBSERVICES_API'].format(
+                five9username=five9username),
+            transport=zeep.Transport(session=session),
+            plugins=[history,]
+        )
+        print(f'Authenticated as {five9username}')
+    except requests.exceptions.HTTPError as e:
+        client = None
+        print(e)
+
+    client.history = history
+
+    return client
