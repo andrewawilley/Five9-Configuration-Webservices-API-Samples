8d38f1e7081e987929ce1103f91ee340ffe3f8fa
Andrew Willey <andrew.willey@five9.com>
2022-06-08 14:41:32 -0500

    domain config capture start

--- five9/domain_config/get_domain_config.py
+++ five9/domain_config/get_domain_config.py
@@ -1,9 +1,9 @@
 import os
-import json
 
-from zeep import helpers
+import git
+import zeep
 
-from five9 import five9_session
+from ..five9 import five9_session
 
 
 client = five9_session.get_client()
@@ -35,30 +35,52 @@ METHODS = [
 class Five9Domain:
     client = None
     domain_objects = {}
-    
+    repo = None
+    vccConfig = None
     
     def __init__(self, client=None):
         self.client = client
-
         if self.client is not None:
             self.get_domain_objects()
     
+    def getVCCConfiguration(self):
+        self.vccConfig = self.client.service.getVCCConfiguration()
+        print(f'Domain {self.vccConfig.domainName}')
+    
+    def get_or_create_repo(self):
+        os.makedirs(os.path.dirname(REPO_PATH), exist_ok=True)
+        repo_path = os.path.dirname(REPO_PATH)
+        try:
+            self.repo = git.Repo(repo_path)
+        except git.exc.InvalidGitRepositoryError as e:
+            print(f'Initializing bare repository in {repo_path}')
+            git.Repo.init(REPO_PATH, bare=False)
+            self.repo = git.Repo(repo_path)
+    
     def get_domain_objects(self):
         if self.client is not None:
-            vccConfig = self.client.service.getVCCConfiguration
-
-            os.makedirs(os.path.dirname(REPO_PATH), exist_ok=True)
+            try:
+                self.getVCCConfiguration()
+                self.get_or_create_repo()
+                branch = self.repo.create_head(self.vccConfig.domainName)
+                self.repo.checkout(branch)
+                print("Obtaining Domain Objects")
+                for method in dir(client.service):
+                    if method in METHODS:
+                        print(f'\t{method}')
+                        vcc_method = getattr(client.service, method)
+                        if method in METHOD_DEFAULT_ARGS.keys():
+                            r = vcc_method(METHOD_DEFAULT_ARGS[method])
+                        else:
+                            try:
+                                r = vcc_method()
+                            except zeep.exceptions.Fault as e:
+                                print(e)
+                        self.domain_objects[method[3:]] = zeep.helpers.serialize_object(r, dict)
                         
-            
-            print("Obtaining Domain Objects")
-            for method in dir(client.service):
-                if method in METHODS:
-                    print(method)
-                    vcc_method = getattr(client.service, method)
-                    if method in METHOD_DEFAULT_ARGS.keys():
-                        r = vcc_method(METHOD_DEFAULT_ARGS[method])
-                    else:
-                        r = vcc_method()
-                    self.domain_objects[method[3:]] = helpers.serialize_object(r, dict)
+            except zeep.exceptions.Fault as e:
+                print(e)
+        else:
+            print('No active client object available to connect with Five9 VCC')
 
 d = Five9Domain(client)
---
+++ five9/sample_domain_config.py
@@ -0,0 +1,5 @@
+print('hello world')
+
+import five9_session
+
+client = five9_session.get_client()
