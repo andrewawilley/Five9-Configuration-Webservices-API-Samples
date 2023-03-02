15dd587f0ed63525b7f7c4d35d2b2536d59dc950
Andrew Willey <andrew.willey@five9.com>
2023-03-02 13:33:21 -0600

    autoformatting with Black

--- five9/domain_config/campaign_profile_comprehension.py
+++ five9/domain_config/campaign_profile_comprehension.py
@@ -9,12 +9,12 @@ def prettify(ugly="", open_set="{[(<", close_set="}])>"):
     level = 0
 
     # Remove any double spaces in the string
-    while ugly.find('  ') > -1:
-        ugly = ugly.replace('  ', ' ')
-    while ugly.find('( (') > -1:
-        ugly = ugly.replace('( (', '((')
-    while ugly.find(') )') > -1:
-        ugly = ugly.replace(') )', '))')
+    while ugly.find("  ") > -1:
+        ugly = ugly.replace("  ", " ")
+    while ugly.find("( (") > -1:
+        ugly = ugly.replace("( (", "((")
+    while ugly.find(") )") > -1:
+        ugly = ugly.replace(") )", "))")
 
     # Initialize a variable to store the formatted output
     output = ""
@@ -24,35 +24,35 @@ def prettify(ugly="", open_set="{[(<", close_set="}])>"):
         # If the character is an opening delimiter, increase the indentation level and add a newline
         if char in open_set:
             level += 1
-            output += char + "\n" + "\t"*(level)
+            output += char + "\n" + "\t" * (level)
         # If the character is a closing delimiter, decrease the indentation level and add a newline
         elif char in close_set:
             level -= 1
-            output += "\n" + "\t"*level + char 
+            output += "\n" + "\t" * level + char
             # If the next character is not a closing delimiter, add an extra newline
-            if ugly[idx+1] not in close_set:
-                output += '\n'
+            if ugly[idx + 1] not in close_set:
+                output += "\n"
         # If the character is "AND" preceded by a space, add a newline and indent
-        elif ugly[idx-1:idx+4] == ' AND ':
-            output += "\n" + "\t"*level+ " "*(-4) + char
+        elif ugly[idx - 1 : idx + 4] == " AND ":
+            output += "\n" + "\t" * level + " " * (-4) + char
         # If the character is "OR" preceded by a space, add a newline and indent
-        elif ugly[idx-1:idx+3] == ' OR ':
-            output += "\n" + "\t"*level+ " "*(-3) + char
+        elif ugly[idx - 1 : idx + 3] == " OR ":
+            output += "\n" + "\t" * level + " " * (-3) + char
         # For all other characters, simply add them to the output string
         else:
             output += char
-    
+
     # Return the formatted string
     return output
 
 
 def demystify_filter(profile_filter, verbose=False):
-    '''
+    """
     profile_filter = profile filter information object with a ["grouping"]["expression"] attribute
     verbose=False by default, set True to spool output to console
-    '''
+    """
     # Compile a regular expression to match numbers
-    numbers = re.compile(r'(\d+)')
+    numbers = re.compile(r"(\d+)")
 
     # Set up placeholder strings for parentheses
     parens_open = "|||||"
@@ -66,52 +66,64 @@ def demystify_filter(profile_filter, verbose=False):
         "Less": "<",
         "LessOrEqual": "<=",
         "Greater": ">",
-        "GreaterOrEqual": ">="
+        "GreaterOrEqual": ">=",
     }
 
     # Get the grouping expression from the profile filter object
     grouping_expression = profile_filter["grouping"]["expression"]
 
     # Replace numbers in the expression with brackets
-    grouping_expression = numbers.sub(r'[\1]', grouping_expression)
+    grouping_expression = numbers.sub(r"[\1]", grouping_expression)
 
     # Iterate through each criteria in the filter
     for idx, criteria in enumerate(profile_filter["crmCriteria"]):
         # Get the index of the current criteria
-        i = idx+1
+        i = idx + 1
 
         # Replace parentheses in the right value with placeholder strings
-        rightValue = (criteria["rightValue"] or "null").replace("(", parens_open).replace(")", parens_close)
+        rightValue = (
+            (criteria["rightValue"] or "null")
+            .replace("(", parens_open)
+            .replace(")", parens_close)
+        )
 
         # Construct the condition string using the left value, compare operator, and right value
-        condition = f'{criteria["leftValue"]} ::{criteria["compareOperator"]}:: {rightValue}'
+        condition = (
+            f'{criteria["leftValue"]} ::{criteria["compareOperator"]}:: {rightValue}'
+        )
 
         # Replace the bracketed number in the grouping expression with the condition string
-        grouping_expression = grouping_expression.replace(f'[{i}]', f'[{condition}][{i}]')
+        grouping_expression = grouping_expression.replace(
+            f"[{i}]", f"[{condition}][{i}]"
+        )
 
     # Use the prettify function to format the grouping expression
-    demystified = prettify(grouping_expression, "(", ")").replace(parens_open, "(").replace(parens_close, ")")
+    demystified = (
+        prettify(grouping_expression, "(", ")")
+        .replace(parens_open, "(")
+        .replace(parens_close, ")")
+    )
 
     # If verbose is True, print the demystified expression to the console
     if verbose == True:
-        print(f'{demystified}')
-    
+        print(f"{demystified}")
+
     # Return the demystified expression
     return demystified
 
 
 def remystify_filter_in_place(nice_filter):
-     nice_filter = re.sub(r"\[.*?\]\[", "", nice_filter)
-     nice_filter = nice_filter.replace("]", "").replace('\t', "").replace("\n", "")
-     return nice_filter
+    nice_filter = re.sub(r"\[.*?\]\[", "", nice_filter)
+    nice_filter = nice_filter.replace("]", "").replace("\t", "").replace("\n", "")
+    return nice_filter
 
 
 def remystify_filter(nice_filter):
     # Remove all bracketed numbers from the nice filter string
-    nice_filter = re.sub(r'(\[([0-9]*)\])', '', nice_filter)
+    nice_filter = re.sub(r"(\[([0-9]*)\])", "", nice_filter)
 
     # Compile a regular expression to match strings enclosed in brackets
-    condition_pattern = re.compile('\[(.*?)\]')
+    condition_pattern = re.compile("\[(.*?)\]")
 
     # Find all occurrences of the condition pattern in the nice filter string
     conditions = condition_pattern.finditer(nice_filter)
@@ -154,30 +166,34 @@ def remystify_filter(nice_filter):
             rightValue = None
 
         # Add a new crmCriteria object to the list
-        crmCriteria.append({
-            "compareOperator": compareOperator,
-            "leftValue": leftValue,
-            "rightValue": rightValue
-        })
+        crmCriteria.append(
+            {
+                "compareOperator": compareOperator,
+                "leftValue": leftValue,
+                "rightValue": rightValue,
+            }
+        )
 
     # Replace all occurrences of the unique conditions in the new grouping expression with the index of the condition
     for idx, condition in enumerate(unique_conditions):
-        new_grouping_expression = re.sub(condition, f'{idx+1}', new_grouping_expression)
+        new_grouping_expression = re.sub(condition, f"{idx+1}", new_grouping_expression)
 
     # Remove brackets and newlines from the new grouping expression, and trim leading/trailing whitespace
-    new_grouping_expression = new_grouping_expression.replace(
-        '[', '').replace(
-        ']', '').replace(
-        '\n', ' ').replace(
-        '\t', ' ').strip()
+    new_grouping_expression = (
+        new_grouping_expression.replace("[", "")
+        .replace("]", "")
+        .replace("\n", " ")
+        .replace("\t", " ")
+        .strip()
+    )
 
     # Remove any double spaces from the new grouping expression
-    while new_grouping_expression.find('  ') > -1:
-        new_grouping_expression = new_grouping_expression.replace('  ', ' ')
+    while new_grouping_expression.find("  ") > -1:
+        new_grouping_expression = new_grouping_expression.replace("  ", " ")
 
     # Print the index and value of each unique condition
     for idx, condition in enumerate(unique_conditions):
-        print(f'{idx+1:02} {condition}')
+        print(f"{idx+1:02} {condition}")
 
     # for idx, condition in enumerate(crmCriteria):
     #     print(f'{idx+1:02} {condition}')
@@ -186,9 +202,6 @@ def remystify_filter(nice_filter):
 
     return {
         "crmCriteria": crmCriteria,
-        "grouping": {
-            "expression": new_grouping_expression,
-            "type": "Custom"
-        },
-        "orderByFields": []
+        "grouping": {"expression": new_grouping_expression, "type": "Custom"},
+        "orderByFields": [],
     }
--- five9/util.py
+++ five9/util.py
@@ -4,9 +4,16 @@ import random
 import re
 import string
 
-def get_random_password(length=20, required_digits=2, required_lower=2, required_caps=2, required_special=1, \
-                                            characters_to_avoid=["O", "0", "=", "|", " ", "\"", "'", ",", "`" ]):
-    '''
+
+def get_random_password(
+    length=20,
+    required_digits=2,
+    required_lower=2,
+    required_caps=2,
+    required_special=1,
+    characters_to_avoid=["O", "0", "=", "|", " ", '"', "'", ",", "`"],
+):
+    """
     Return a randomly generated password of a specified length and composition
 
     Keyword arguments:
@@ -15,35 +22,62 @@ def get_random_password(length=20, required_digits=2, required_lower=2, required
     required_lower  -- minimum number of password characters that are lowercase a-z, default 2
     required_caps   -- minimum number of password characters that are uppercase A-Z, default 2
     required_special-- minimum number of password characters that are non-alphanumeric, default 2
-    
-    characters_to_avoid -- list of characters that should be excluded from the password, 
+
+    characters_to_avoid -- list of characters that should be excluded from the password,
                            default = ["O", "0", "=", "|", " ", "\"", "'", ",", "`" ]
-        
-    '''
+
+    """
     random.seed()
-    
-    password_characters =(string.ascii_letters + string.digits + string.punctuation).translate(
-        {ord(x): '' for x in characters_to_avoid})
 
-    password_lower = ''.join(random.choice(string.ascii_letters.lower().translate(
-        {ord(x): '' for x in characters_to_avoid})) for i in range(required_lower)) 
-    
-    password_caps = ''.join(random.choice(string.ascii_letters.upper().translate(
-        {ord(x): '' for x in characters_to_avoid})) for i in range(required_caps))
-    
-    password_digits = ''.join(random.choice(string.digits.translate(
-        {ord(x): '' for x in characters_to_avoid})) for i in range(required_digits))    
-    
-    password_spec =  ''.join(random.choice(string.punctuation.translate(
-        {ord(x): '' for x in characters_to_avoid})) for i in range(required_special))
+    password_characters = (
+        string.ascii_letters + string.digits + string.punctuation
+    ).translate({ord(x): "" for x in characters_to_avoid})
 
-    rnd_char_count = length - required_digits - required_lower - required_caps - required_special
+    password_lower = "".join(
+        random.choice(
+            string.ascii_letters.lower().translate(
+                {ord(x): "" for x in characters_to_avoid}
+            )
+        )
+        for i in range(required_lower)
+    )
 
-    password_base = ''.join(random.choice(password_characters) for i in range(rnd_char_count))
-    password = password_base + password_lower + password_caps + password_digits + password_spec
+    password_caps = "".join(
+        random.choice(
+            string.ascii_letters.upper().translate(
+                {ord(x): "" for x in characters_to_avoid}
+            )
+        )
+        for i in range(required_caps)
+    )
+
+    password_digits = "".join(
+        random.choice(
+            string.digits.translate({ord(x): "" for x in characters_to_avoid})
+        )
+        for i in range(required_digits)
+    )
+
+    password_spec = "".join(
+        random.choice(
+            string.punctuation.translate({ord(x): "" for x in characters_to_avoid})
+        )
+        for i in range(required_special)
+    )
+
+    rnd_char_count = (
+        length - required_digits - required_lower - required_caps - required_special
+    )
+
+    password_base = "".join(
+        random.choice(password_characters) for i in range(rnd_char_count)
+    )
+    password = (
+        password_base + password_lower + password_caps + password_digits + password_spec
+    )
     password = list(password)
     random.shuffle(password)
-    password = ''.join(password)
+    password = "".join(password)
     # print(f'{password_base} {password_lower} {password_caps} {password_digits} {password_spec} = {password}')
     return password
 
@@ -53,17 +87,19 @@ def ivr_variable_usage(ivrs, verbose=False):
     ivr_variables = {}
 
     # Compile a regular expression to find script variables in the xmlDefinition attribute
-    script_variable_pattern = re.compile(r'(?<=<variableName>)(.*?)(?=<\/variableName>)')
-    
+    script_variable_pattern = re.compile(
+        r"(?<=<variableName>)(.*?)(?=<\/variableName>)"
+    )
+
     # Iterate over the IVR objects
     for ivr in ivrs:
         # Skip IVRs with "EXAMPLE" in their name
-        if 'EXAMPLE' in ivr.name:
+        if "EXAMPLE" in ivr.name:
             continue
-        
+
         # Find all instances of script variables in the xmlDefinition attribute
         script_variables = script_variable_pattern.finditer(ivr.xmlDefinition)
-        
+
         # Iterate over the script variables
         for var in script_variables:
             # Extract the variable name
@@ -72,7 +108,7 @@ def ivr_variable_usage(ivrs, verbose=False):
             variable = ivr.xmlDefinition[b:e]
             # Split the variable name into parts at the period
             variable_parts = variable.split(".")
-            
+
             # If the variable has more than one part, add it to the ivr_variables dictionary
             if len(variable_parts) > 1:
                 if ivr_variables.get(variable, None) == None:
@@ -82,7 +118,7 @@ def ivr_variable_usage(ivrs, verbose=False):
 
     # Sort the ivr_variables dictionary
     ivr_variables = collections.OrderedDict(sorted(ivr_variables.items()))
-    
+
     # If verbose is True, print the ivr_variables dictionary as a JSON object
     if verbose == True:
         j = json.dumps(ivr_variables, indent=4)
