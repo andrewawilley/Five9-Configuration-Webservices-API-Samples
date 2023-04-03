854cb2737bb3f0b2c23130586df22d5a1f216ceb
Andrew Willey <andrew.willey@five9.com>
2023-04-03 17:37:52 -0500

    Added git tracking to the domain capture

--- five9/tests/testDomainCapture.py
+++ five9/tests/testDomainCapture.py
@@ -36,6 +36,18 @@ class TestDomainCapture(unittest.TestCase):
         self.account = "default_test_account"
         self.client = Five9Client(account=self.account)
 
+    def initialize_domain_configuration(self, get_objects=False):
+        # if no self.domain_configuration object exists, create one
+        if not hasattr(self, "domain_configuration"):
+            self.domain_configuration = Five9DomainConfig(
+                client=self.client,
+                # methods=["getCampaignProfiles"]
+            ) 
+            get_objects = True
+        
+        if get_objects:
+            self.domain_configuration.get_domain_objects()
+
 
     def test_domain_config_capture(self):
         self.domain_configuration = Five9DomainConfig(
@@ -43,15 +55,20 @@ class TestDomainCapture(unittest.TestCase):
             # methods=["getCampaignProfiles"]
         )
         
-        self.assertEqual(self.domain_configuration.client, self.client)
+        # self.assertEqual(self.domain_configuration.client, self.client)
 
         # assert that a folder was created matching the domain name in the
         # domain_config/domain_snapshots folder
         self.assertTrue(os.path.exists(self.domain_configuration.domain_path))
 
+        # assert that there is a git repo in the domain_config/domain_snapshots folder
+        self.assertTrue(os.path.exists(os.path.join(self.domain_configuration.domain_path, ".git")))        
+
+    def test_domain_config_capture_campaign_profiles(self):
         # assert that a a demystified campaign profile was created for each
         # campaign in the domain with type "OUTBOUND" that also has a campaignProfile
         # with one or more crmCriteria
+        self.initialize_domain_configuration()
 
         for campaign in self.domain_configuration.domain_objects["getCampaigns"]:
             if (
