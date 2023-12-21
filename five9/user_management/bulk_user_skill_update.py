0b144a0f077bddf8c80e2bd0abc87f02af8cdd6a
Andrew Willey <andrew.willey@five9.com>
2023-12-21 09:44:45 -0600

    updates and additions to user management function samples

--- five9/user_management/bulk_user_skill_update.py
+++ five9/user_management/bulk_user_skill_update.py
@@ -1,82 +1,84 @@
 import csv
 import datetime
 import time
-
 import zeep
-
+from tqdm import tqdm
 import five9_session
 
+def manage_user_skills(client, users_to_update, skills_to_add, skills_to_remove):
+    """
+    Manages skills for specified users in the Five9 domain.
 
-client = five9_session.Five9Client()
+    This function updates the skills of specified users by adding and removing skills
+    based on provided lists. It uses the Five9 domain's service to make these updates.
 
-# populate a list of usernames to manage the skills for
-users_to_update = [
-    "testguy3@aw_tam",
-]
+    Parameters:
+    client: Zeep Client object
+        The client used to interact with the Five9 domain.
+    users_to_update: list of str
+        List of usernames for which skills will be managed.
+    skills_to_add: list of str
+        Skill names to be added to the users.
+    skills_to_remove: list of str
+        Skill names to be removed from the users.
 
-# populate a list of skills to add, and another list of skills to remove
-skill_names_to_add = ["outbound_test", "outreach_en"]
+    Returns:
+    tuple: (updated_count, error_count)
+        updated_count: The number of successful updates.
+        error_count: The number of errors encountered during updates.
 
-skill_names_to_remove = ["test"]
+    Raises:
+    zeep.exceptions.Fault
+        If an error occurs during the skill management process.
+    """
 
-skills_to_add = []
-skills_to_remove = []
+    skills_add_objs = []
+    skills_remove_objs = []
 
-for skill in skill_names_to_add:
-    try:
-        skills_to_add.append(client.service.getSkill(skill))
-    except zeep.exceptions.Fault as e:
-        print(f"{e}")
-
-for skill in skill_names_to_remove:
-    try:
-        skills_to_remove.append(client.service.getSkill(skill))
-    except zeep.exceptions.Fault as e:
-        print(f"{e}")
-
-
-errors = 0
-updated = 0
-
-target_updates = len(users_to_update)
-
-
-for user_to_update in users_to_update[updated:]:
-    for target_skill_to_add in skills_to_add:
-        user_skill_to_add = {
-            "id": target_skill_to_add.id,
-            # this could be set dynamically, hardcoded in this example to the highest level (1)
-            "level": 1,
-            "skillName": target_skill_to_add.name,
-            "userName": user_to_update,
-        }
-
+    for skill_name in skills_to_add:
         try:
-            skill_added = client.service.userSkillAdd(userSkill=user_skill_to_add)
-            # force half second delay to avoid rate limits
-            time.sleep(0.5)
+            skills_add_objs.append(client.service.getSkill(skill_name))
         except zeep.exceptions.Fault as e:
-            print(f"{e}")
+            print(f"Error retrieving skill '{skill_name}': {e}")
 
-    for target_skill_to_remove in skills_to_remove:
-        user_skill_to_remove = {
-            "id": target_skill_to_remove.id,
-            "skillName": target_skill_to_remove.name,
-            "level": 1,
-            "userName": user_to_update,
-        }
-
+    for skill_name in skills_to_remove:
         try:
-            skill_removed = client.service.userSkillRemove(
-                userSkill=user_skill_to_remove
-            )
-            # force half second delay to avoid rate limits
-            time.sleep(0.5)
+            skills_remove_objs.append(client.service.getSkill(skill_name))
         except zeep.exceptions.Fault as e:
-            print(f"{e}")
+            print(f"Error retrieving skill '{skill_name}': {e}")
 
-    updated += 1
-    print(f"Updated:\t{updated}/{target_updates} \tErrors: {errors}", end="\r")
-    time.sleep(0.5)
+    error_count = 0
+    updated_count = 0
 
-print(f"Updated:\t{updated}/{target_updates} \tErrors: {errors}")
+    with tqdm(total=len(users_to_update), desc="Updating user skills", mininterval=1) as pbar:
+        for user in users_to_update:
+            try:
+                for skill in skills_add_objs:
+                    user_skill = {
+                        "id": skill.id,
+                        "level": 1,  # Assuming the highest level
+                        "skillName": skill.name,
+                        "userName": user,
+                    }
+                    client.service.userSkillAdd(userSkill=user_skill)
+                    time.sleep(0.5)  # Delay to avoid rate limits
+
+                for skill in skills_remove_objs:
+                    user_skill = {
+                        "id": skill.id,
+                        "level": 1,
+                        "skillName": skill.name,
+                        "userName": user,
+                    }
+                    client.service.userSkillRemove(userSkill=user_skill)
+                    time.sleep(0.5)  # Delay to avoid rate limits
+
+                updated_count += 1
+            except zeep.exceptions.Fault as e:
+                error_count += 1
+                print(f"Error updating skills for user '{user}': {e}")
+            finally:
+                pbar.update(1)
+                pbar.set_postfix({"Updated": updated_count, "Errors": error_count})
+
+    return updated_count, error_count
