9ef8f6628b6877e5b3be8748a5c060e408f60d28
andrewawilley <andrew.willey@five9.com>
2025-04-23 08:39:32 -0500

    introduce common cmdline args for examples

--- examples/domain_config/contact_field_removal.py
+++ examples/domain_config/contact_field_removal.py
@@ -1,9 +1,7 @@
-import argparse
 import os
 import time
-
-from five9 import five9_session
 import tqdm
+from five9.utils.common import parse_arguments, create_five9_client
 
 
 def get_campaigns_by_campaign_status(client, target_status):
@@ -19,65 +17,21 @@ def get_campaigns_by_campaign_status(client, target_status):
 
 # if main script
 if __name__ == "__main__":
-
-    # get the list of fields to remove from the argument "fields_to_remove"
-    parser = argparse.ArgumentParser(
-        description="Remove specific contact fields from Five9"
-    )
-
-    parser.add_argument(
-        "--username",
-        metavar="Five9 Username",
-        default=None,
-        type=str,
-        required=False,
-        help="Username for Five9 account with the admin/api role",
-    )
-
-    parser.add_argument(
-        "--password",
-        metavar="Five9 Password",
-        default=None,
-        type=str,
-        required=False,
-        help="Password for the Five9 account",
-    )
-
-    parser.add_argument(
-        "--account_alias",
-        metavar="Stored credential alias",
-        default=None,
-        type=str,
-        required=False,
-        help="Alias for a stored credential object in private/credentials.py",
-    )
-
-    parser.add_argument(
-        "--hostalias",
-        type=str,
-        default="us",
-        help="Five9 host alias (us, ca, eu, frk, in)",
-    )
-
-    parser.add_argument(
-        "--fields_to_remove",
-        type=str,
-        required=True,
-        help="List of contact fields to remove",
-    )
-    args = parser.parse_args()
+    additional_args = [
+        {
+            "name": "--fields_to_remove",
+            "type": str,
+            "required": True,
+            "help": "List of contact fields to remove (pipe delimited)",
+        }
+    ]
+    args = parse_arguments(additional_args)
 
     # create a list from the fields to remove, assume they are pipe-delimited
     fields_to_remove = args.fields_to_remove.split("|")
 
     # Create Five9 client
-    client = five9_session.Five9Client(
-        five9username=args.username,
-        five9password=args.password,
-        account=args.account_alias,
-        api_hostname_alias=args.hostalias,
-        methods=["getCampaignProfiles"],
-    )
+    client = create_five9_client(args)
 
     # Fetch all running campaigns
     originally_running_campaigns = get_campaigns_by_campaign_status(client, "RUNNING")
@@ -85,7 +39,7 @@ if __name__ == "__main__":
     if originally_running_campaigns:
         # create the /private subfolder if it doesn't exist
         os.makedirs("private", exist_ok=True)
-        
+
         # write the list of running campaigns to a file in the /private subfolder
         with open("private/running_campaigns.txt", "w") as f:
             for campaign in originally_running_campaigns:
@@ -100,7 +54,7 @@ if __name__ == "__main__":
             for campaign in originally_running_campaigns:
                 try:
                     client.service.stopCampaign(campaign.name)
-                    time.sleep(.3)
+                    time.sleep(0.3)
                 except Exception as e:
                     print(f"\nFAILED to stop campaign '{campaign.name}': {e}\n")
                 finally:
@@ -115,17 +69,25 @@ if __name__ == "__main__":
         campaigns_in_stopping = get_campaigns_by_campaign_status(client, "STOPPING")
         campaigns_in_stopping_count = len(campaigns_in_stopping)
         if len(campaigns_in_stopping) > 0:
-            print(f'\n{campaigns_in_stopping_count} campaigns are still stopping. Waiting for them to stop...\n')
+            print(
+                f"\n{campaigns_in_stopping_count} campaigns are still stopping. Waiting for them to stop...\n"
+            )
             with tqdm.tqdm(
                 total=campaigns_in_stopping_count,
                 desc="Monitoring STOPPING status campaigns (5 seconds)",
                 mininterval=1,
             ) as pbar:
                 while len(campaigns_in_stopping) > 0:
-                    campaigns_in_stopping = get_campaigns_by_campaign_status(client, "STOPPING")
+                    campaigns_in_stopping = get_campaigns_by_campaign_status(
+                        client, "STOPPING"
+                    )
                     campaigns_in_stopping_still_count = len(campaigns_in_stopping)
-                    pbar.set_postfix({"Campaigns still STOPPING": campaigns_in_stopping_still_count})
-                    pbar.update(campaigns_in_stopping_count - campaigns_in_stopping_still_count)
+                    pbar.set_postfix(
+                        {"Campaigns still STOPPING": campaigns_in_stopping_still_count}
+                    )
+                    pbar.update(
+                        campaigns_in_stopping_count - campaigns_in_stopping_still_count
+                    )
                     time.sleep(5)
 
         print("\nAll campaigns are stopped.\n")
@@ -138,8 +100,8 @@ if __name__ == "__main__":
                 print(f"\tDeleted field '{field}'")
             except Exception as e:
                 print(f"\tFAILED to delete field '{field}': {e}\n")
-        
-        print('\nAll targeted fields removed.\n')
+
+        print("\nAll targeted fields removed.\n")
         # restart all previously running campaigns
         with tqdm.tqdm(
             total=len(originally_running_campaigns),
@@ -149,7 +111,7 @@ if __name__ == "__main__":
             for campaign in originally_running_campaigns:
                 try:
                     client.service.startCampaign(campaign.name)
-                    time.sleep(.3)
+                    time.sleep(0.3)
                 except Exception as e:
                     print(f"\nFAILED to start campaign '{campaign.name}': {e}\n")
                 finally:
@@ -159,13 +121,17 @@ if __name__ == "__main__":
         print("\nNo running campaigns found.\n")
 
     # get the list of running campaigns
-    running_campaigns_after_process = get_campaigns_by_campaign_status(client, "RUNNING")
+    running_campaigns_after_process = get_campaigns_by_campaign_status(
+        client, "RUNNING"
+    )
 
     if len(running_campaigns_after_process) == len(originally_running_campaigns):
         print("\nAll campaigns restarted successfully.\n")
     else:
         print("\nSome campaigns failed to restart.\n")
         # print the list of campaigns that failed to restart
-        failed_campaigns = list(set(originally_running_campaigns) - set(running_campaigns_after_process))
+        failed_campaigns = list(
+            set(originally_running_campaigns) - set(running_campaigns_after_process)
+        )
         for campaign in failed_campaigns:
             print(f"\t{campaign.name}")
